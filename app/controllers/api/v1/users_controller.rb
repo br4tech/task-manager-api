34c20f9ed63c4de7420a9ccc860d52f5c2a6aa1d
Costa Nonato <costa@nonato.com>
2018-02-25 22:26:19 +0000

    Adding authorization for the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-
+  # Verifica se existe um usuario na apolicação, caso não tiver encontrado retornado o 201
+  before_action :authenticate_with_token!, only:[:update, :destroy]
   respond_to :json
 
   def show
@@ -23,7 +24,9 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    # user = User.find(params[:id])
+    # Agora so vai pegar o usuario da "sessao" para atualizar
+    user = current_user
 
     if user.update(user_params)
       render json: user, status: 200
@@ -33,14 +36,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
-    user  = User.find(params[:id])
-    user.destroy
+    # user  = User.find(params[:id])
+    current_user.destroy
     head 204
   end
 
   private
   def user_params
-    params.permit  [:email, :password, :password_confirmation]
+        params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 
