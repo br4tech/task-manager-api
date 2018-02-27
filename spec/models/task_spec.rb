81d3d67a41836b3096855ceaa64797a5b4ad7530
Costa Nonato <costa@nonato.com>
2018-02-27 22:54:58 +0000

    Adding test and improvements for task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:task){ build(:task)}
+  context 'when is new' do
+    # Verifica se o valor do campo done é verdadeiro/falso, qualquer campo que for boleano be_campo_boleano
+     it{ expect(task).not_to be_done }
+  end
+  # Verifica se o relacionamento existe
+   it{ is_expected.to belong_to(:user) }
+
+   it{ is_expected.to validate_presence_of :title }
+   it{ is_expected.to validate_presence_of :user_id }
+
+  # Verifica se existe os campos para ver se o campo não foi removido
+   it{ is_expected.to respond_to(:title)}
+   it{ is_expected.to respond_to(:description)}
+   it{ is_expected.to respond_to(:deadline)}
+   it{ is_expected.to respond_to(:done)}
+   it{ is_expected.to respond_to(:user_id)}
+
 end
