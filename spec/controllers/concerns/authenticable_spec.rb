984584f3e3c1f33c73f303244021031bae88ecc2
Costa Nonato <costa@nonato.com>
2018-02-27 01:14:15 +0000

    Refactoring the authorization and adding user_singned_in? method

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -51,6 +51,27 @@ RSpec.describe Authenticable do
       end
 
     end
+
+    describe '#user_logged_in?' do
+
+      context 'when there is a user logger in' do
+        before do
+           user = create(:user)
+           allow(app_controller).to receive(:current_user).and_return(user)
+        end
+
+        it{ expect(app_controller.user_logged_in?).to be true }
+      end
+
+      context 'when there is no user logger in' do
+        before do
+           user = create(:user)
+           allow(app_controller).to receive(:current_user).and_return(nil)
+        end
+
+        it{ expect(app_controller.user_logged_in?).to be false}
+      end
+    end
 end
 
 # def current_user
