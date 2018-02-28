1a9cf56d54540671b388abbd74191da7ddbaa1e8
Costa Nonato <costa@nonato.com>
2018-02-28 00:28:38 +0000

    Adding create action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,13 +2,31 @@ class Api::V1::TasksController < ApplicationController
   before_action :authenticate_with_token!
 
   def index
-     tasks = current_user.tasks
-     render json: { tasks: tasks }, status: 200
+    tasks = current_user.tasks
+    render json: { tasks: tasks }, status: 200
   end
 
   def show
     task = current_user.tasks.find(params[:id])
     render json: task, status: 200
+  end
+
+  def create
+    # Build gera uma nova task
+    # Exemplo: task  = Tasks.new(user_id: current_user.id)
+    task = current_user.tasks.build(task_params)
+
+    if task.save
+      render json: task, status: 201
+    else
+      render json: {errors: task.errors}, status: 422
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :done)
   end
 
 end
--- spec/request/api/v1/tasks_spec.rb
+++ spec/request/api/v1/tasks_spec.rb
@@ -43,4 +43,49 @@ RSpec.describe 'Tasks API', type: :request do
      end
 
   end
+
+  describe 'POST /tasks' do
+    before do
+      post '/tasks', params:{ task: task_params }.to_json, headers: headers
+    end
+
+    context 'when the params are valid' do
+      let(:task_params){ attributes_for(:task) }
+
+      it 'returns status code 201' do
+         expect(response).to have_http_status(201)
+      end
+
+      # Verifica se existe um registro com o titulo no database
+      it 'saves the task in the database' do
+        expect(Task.find_by(title: task_params[:title])).not_to be_nil
+      end
+
+      it 'return the json for create task' do
+         expect(json_body[:title]).to eq(task_params[:title])
+      end
+
+      it 'assings the create taks to the current user' do
+        expect(json_body[:user_id]).to eq(user.id)
+      end
+    end
+
+    context 'when the params are invalid' do
+     let(:task_params){ attributes_for(:task, title: ' ') }
+
+     it 'return status code 422' do
+       expect(response).to have_http_status(422)
+     end
+
+     it 'does not save the task in the database' do
+          expect(Task.find_by(title: task_params[:title])).to be_nil
+     end
+
+     it 'return the json error for title' do
+      expect(json_body[:errors]).to have_key(:title)
+     end
+
+    end
+
+  end
 end
